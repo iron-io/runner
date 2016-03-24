cc12298d654a0b69f2a50c5bcdd19d9047f04c88
Derek Schultz <schultz.derek@gmail.com>
2016-03-24 14:58:22 -0600

    driver/docker: start porting gorunner Docker driver to Titan

--- drivers/driver.go
+++ drivers/driver.go
@@ -2,10 +2,37 @@
 
 package drivers
 
+import "os"
+
 type Driver interface {
-	Run(*models.Job) RunResult
+	// Run(*models.Job) RunResult
+	Run(task ContainerTask, isCancelled chan bool) RunResult
 }
 
 // RunResult will provide methods to access the job completion status, logs, etc.
 type RunResult interface {
+	// Err() is an actionable/checkable error from the container.
+	Error() error
+
+	// Status() should return the current status of the task.
+	// It must never return Enqueued.
+	Status() string
+
+	// Log() will be a Reader interface that allows the driver to read the content
+	// of the log output for the task that was run.
+	// Each driver is free to implement this in its own way, either streaming or at
+	// the end of execution.
+	//
+	// It must return a valid Reader at any time.
+	Log() *os.File // TODO: change to io.Reader
+}
+
+type ContainerTask interface {
+	Command() string
+	Config() string
+	EnvVars() map[string]string
+	Id() string
+	Image() string
+	Payload() string
+	Timeout() uint
 }
