6427e81ad2ec938b2fd1742927eff494f7d1b51b
Travis Reeder <treeder@gmail.com>
2016-08-05 17:43:23 -0400

    Added sleep to dind Docker to ensure docker daemon has started.

--- dind/release.sh
+++ dind/release.sh
@@ -12,6 +12,7 @@ cd go-dind
 echo "go-dind pwd $PWD"
 docker tag iron/go-dind:latest iron/go-dind:$version
 
-
-docker push iron/dind
-docker push iron/go-dind
+docker push iron/dind:latest
+docker push iron/dind:$version
+docker push iron/go-dind:latest
+docker push iron/go-dind:$version
