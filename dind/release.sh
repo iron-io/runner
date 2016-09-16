d9569f663f14035c23e6d35697a97b533478a707
Reed Allman <rdallman10@gmail.com>
2016-09-16 10:09:37 -0700

    remove go-dind. die like a nic cage movie. please stop this docker madness

--- dind/release.sh
+++ dind/release.sh
@@ -8,11 +8,5 @@ echo "version $version"
 
 docker tag iron/dind:latest iron/dind:$version
 
-cd go-dind
-echo "go-dind pwd $PWD"
-docker tag iron/go-dind:latest iron/go-dind:$version
-
 docker push iron/dind:latest
 docker push iron/dind:$version
-docker push iron/go-dind:latest
-docker push iron/go-dind:$version
