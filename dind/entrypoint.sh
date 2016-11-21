e05584bd19c63f9b3267807f86c170d907919f50
Reed Allman <rdallman10@gmail.com>
2016-11-20 22:54:52 -0600

    reboot dind when docker dies (#15)
    * reboot dind when docker dies
    
    now that docker-containerd is a thing we should be able to reboot docker and
    resume processing task containers without having to error the tasks out in a
    lot of cases (looking at you, oom)
    
    * add killing of libcontainer so that docker actually reboots

--- dind/entrypoint.sh
+++ dind/entrypoint.sh
@@ -15,20 +15,8 @@
 # limitations under the License.
 
 set -ex
-# modified from: https://github.com/docker-library/docker/blob/866c3fbd87e8eeed524fdf19ba2d63288ad49cd2/1.11/dind/dockerd-entrypoint.sh
-# this will run either overlay or aufs as the docker fs driver, if the OS has both, overlay is preferred.
-# rewrite overlay to use overlay2 (docker 1.12, linux >=4.x required), see https://docs.docker.com/engine/userguide/storagedriver/selectadriver/#overlay-vs-overlay2
 
-fsdriver=$(grep -Eh -w -m1 "overlay|aufs" /proc/filesystems | cut -f2)
-
-if [ $fsdriver == "overlay" ]; then
-  fsdriver="overlay2"
-fi
-
-docker daemon \
-		--host=unix:///var/run/docker.sock \
-		--host=tcp://0.0.0.0:2375 \
-		--storage-driver=$fsdriver &
+/usr/local/bin/dind.sh &
 
 # wait for daemon to initialize
 sleep 3
